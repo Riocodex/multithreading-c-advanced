da404b1d7a536018a19c415025fd644df67cd7ed
Riocodex <onwukachibike@gmail.com>
2023-10-20 19:16:23 +0200

    updated

--- sum.c
+++ sum.c
@@ -1,45 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <time.h>
 #include <unistd.h>
 
-int primes[10] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+int primes[10] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
 
-void* routine(void* arg)
-{
-    //  sleep(1);
-     int index = *(int*) arg; 
-     printf("%d ",primes[index]);
-     free(arg);
-     return NULL;
+void* routine(void* arg) {
+    int index = *(int*)arg;
+    int sum = 0;
+    for (int j = 0; j < 5; j++) {
+        sum += primes[index + j];
+    }
+    printf("Local sum: %d\n", sum);
+    *(int*)arg = sum;
+    return arg;
 }
-   
 
-
-int main(int argc, char* argv[])
-{
-    int n = 2;
-    pthread_t th[n];
-
-    for (int i = 0; i < n; i++)
-    {
+int main(int argc, char* argv[]) {
+    pthread_t th[2];
+    int i;
+    for (i = 0; i < 2; i++) {
         int* a = malloc(sizeof(int));
-        *a = i;
-        if (pthread_create(&th[i], NULL,&routine,a) != 0)
-        {
-            return 1;
-        }
-        
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        if(pthread_join(th[i], NULL) != 0){
-            perror("failed to join thread");
+        *a = i * 5;
+        if (pthread_create(&th[i], NULL, &routine, a) != 0) {
+            perror("Failed to create thread");
         }
     }
-    
-       
+    int globalSum = 0;
+    for (i = 0; i < 2; i++) {
+        int* r;
+        if (pthread_join(th[i], (void**) &r) != 0) {
+            perror("Failed to join thread");
+        }
+        globalSum += *r;
+        free(r);
+    }
+    printf("Global sum: %d\n", globalSum);
     return 0;
 }
